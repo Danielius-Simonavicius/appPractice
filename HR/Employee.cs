b956b73913077be11ddd35c05df3dcc0d4e2979c
Danielius-Simonavicius <Daniel@theguy.xyz>
2024-02-21 14:58:11 +0000

    added properties and validation logic for changing wages
    added properties/validation logic when setting persons wage

--- HR/Employee.cs
+++ HR/Employee.cs
@@ -7,30 +7,44 @@ namespace appPractice;
 
 internal class Employee
 {
-    private string firstName;
-    private string lastName;
+    public string firstName;
+    public string lastName;
     private string email;
-    public int numberOfHoursWorked;
-    public double wage;
-    public double hourlyRate;
+    private int numberOfHoursWorked;
+    private double wage;
+    private double hourlyRate;
     public DateTime birthday;
 
     const int minimalHoursWorkedUnit = 1;
 
     //constructor 
-    public Employee(string first, string last, string em, DateTime bd, double rate)
+    public Employee(string first, string last, string em, DateTime bd, double? rate)
     {
         firstName = first;
         lastName = last;
         email = em;
         birthday = bd;
-        hourlyRate = rate;
+        hourlyRate = rate ?? 10;
+    }
+
+    public double? HourlyRate
+    {
+        get { return hourlyRate; }
+        set 
+        {
+            if(hourlyRate < 0){ //added validation logic to make sure wage cannot be set to minus values
+                hourlyRate = 0;
+            }
+            else{
+                hourlyRate = (double) value;
+            }
+        }
     }
 
     public void PerformWork()
     {
-        numberOfHoursWorked++;
-        Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked} hour(s)!");
+        PerformWork(minimalHoursWorkedUnit);
+        // Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked} hour(s)!");
     }
     public void PerformWork(int numberOfHours)
     {
@@ -38,7 +52,19 @@ internal class Employee
         Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked} hour(s)!");
     }
 
-    public void DisplayEmployeeDetails(){
+    public double RecieveWage(bool resetHours = true)
+    {
+        wage = numberOfHoursWorked * hourlyRate;
+        Console.WriteLine($"{firstName} {lastName} has recieved a wage of {wage} for {numberOfHoursWorked} hour(s) of work.");
+        if (resetHours)
+        {
+            numberOfHoursWorked = 0;
+        }
+        return wage;
+    }
+
+    public void DisplayEmployeeDetails()
+    {
         Console.WriteLine($"\nFirst name: \t{firstName}\nLast name: \t{lastName}\nEmail:\t\t{email}Birthday: \t{birthday.ToShortDateString()}\n");
     }
 }
